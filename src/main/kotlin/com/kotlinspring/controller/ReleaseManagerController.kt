d8464edc2c9a3be668b2716dd3ea1c2c603e9a18
Amin Shahimian <mohammadamin.shahimian@sti-consulting.com>
2023-05-04 09:59:30 +0200

    add ReleaseManagerService and inject in controller

--- src/main/kotlin/com/kotlinspring/controller/ReleaseManagerController.kt
+++ src/main/kotlin/com/kotlinspring/controller/ReleaseManagerController.kt
@@ -1,12 +1,13 @@
 package com.kotlinspring.controller
 
+import com.kotlinspring.service.ReleaseManagerService
 import mu.KLogging
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/v1/releaseManagement")
-class ReleaseManagerController {
+class ReleaseManagerController(val releaseManagerService: ReleaseManagerService) {
 
     companion object : KLogging()
 
---
+++ src/main/kotlin/com/kotlinspring/service/ReleaseManagerService.kt
@@ -0,0 +1,7 @@
+package com.kotlinspring.service
+
+import org.springframework.stereotype.Service
+
+@Service
+class ReleaseManagerService {
+}
