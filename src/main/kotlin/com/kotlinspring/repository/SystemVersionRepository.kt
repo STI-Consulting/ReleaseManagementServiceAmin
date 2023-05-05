13f6508319a21a665a636725bf93b7d24a17c321
Amin Shahimian <mohammadamin.shahimian@sti-consulting.com>
2023-05-05 10:25:00 +0200

    refactor the ReleaseManagerService

--- src/main/kotlin/com/kotlinspring/repository/SystemVersionRepository.kt
+++ src/main/kotlin/com/kotlinspring/repository/SystemVersionRepository.kt
@@ -4,4 +4,5 @@ import com.kotlinspring.entity.SystemVersion
 import org.springframework.data.repository.CrudRepository
 
 interface SystemVersionRepository: CrudRepository<SystemVersion, Int> {
+    abstract fun save(systemEntity: SystemVersion?)
 }
--- src/main/kotlin/com/kotlinspring/service/ReleaseManagerService.kt
+++ src/main/kotlin/com/kotlinspring/service/ReleaseManagerService.kt
@@ -2,36 +2,52 @@ package com.kotlinspring.service
 
 import com.kotlinspring.dto.MicroServiceDto
 import com.kotlinspring.dto.SystemVersionDto
+import com.kotlinspring.entity.MicroService
+import com.kotlinspring.entity.SystemVersion
 import com.kotlinspring.repository.MicroServiceRepository
 import com.kotlinspring.repository.SystemVersionRepository
 import org.springframework.stereotype.Service
 
 @Service
-class ReleaseManagerService(val microServiceRepository: MicroServiceRepository,
-                            val systemVersionRepository: SystemVersionRepository) {
-
-    private var currentSystemVersionDto = SystemVersionDto(0, 0, emptyList())
+class ReleaseManagerService(
+    val microServiceRepository: MicroServiceRepository,
+    val systemVersionRepository: SystemVersionRepository
+) {
 
 
     fun retrieveMicroServicesWithSystemVersion(systemVersion: Int): List<com.kotlinspring.dto.MicroServiceDto> {
-        val microservices = microServiceRepository.findAll().filter{ it.version <= systemVersion }
-       return microservices.map { MicroServiceDto(it.id,it.name,it.version) }
+        val microservices = microServiceRepository.findAll().filter { it.systemVersion?.equals(systemVersion) ?: true }
+        return microservices.map { MicroServiceDto(it.id, it.name, it.version) }
     }
 
     fun updateServiceVersion(microServiceDto: com.kotlinspring.dto.MicroServiceDto): Int {
-        val existingService = currentSystemVersionDto.microServiceDtos.find { it.name == microServiceDto.name }
-        val newServices = if (existingService == null) {
-            currentSystemVersionDto.microServiceDtos + microServiceDto
-        } else {
-            currentSystemVersionDto.microServiceDtos.map { if (it.name == microServiceDto.name) microServiceDto else it }
-        }
-        val newSystemVersionDto = if (existingService == null || existingService.version != microServiceDto.version) {
-            SystemVersionDto(0, currentSystemVersionDto.number + 1, newServices)
-        } else {
-            currentSystemVersionDto
-        }
-        currentSystemVersionDto = newSystemVersionDto
+        val existingService = microServiceRepository.findAll().filter { it.name == microServiceDto.name }
+        if (existingService == null) {
+            increaseSystemVersion(microServiceDto)
 
-        return currentSystemVersionDto.number
+        } else {
+
+            val sameVersion = existingService.filter { it.version == microServiceDto.version }
+            if (sameVersion == null) {
+                increaseSystemVersion(microServiceDto)
+            }
+        }
+        val allSystemVersion = systemVersionRepository.findAll()
+        val lastSystemVersion = allSystemVersion.maxByOrNull { it.number }
+        if (lastSystemVersion != null) {
+            return lastSystemVersion.number
+        }
+        return 0
+    }
+
+    private fun increaseSystemVersion(microServiceDto: MicroServiceDto) {
+        val entity = MicroService(null, microServiceDto.name, microServiceDto.version)
+        microServiceRepository.save(entity)
+        val allSystemVersion = systemVersionRepository.findAll()
+        val currentSystemVersion = allSystemVersion.maxByOrNull { it.number }
+        val newSystemNr = (currentSystemVersion?.number)?.plus(1)
+        val systemEntity = newSystemNr?.let { SystemVersion(null, newSystemNr) }
+
+        systemVersionRepository.save(systemEntity)
     }
 }
